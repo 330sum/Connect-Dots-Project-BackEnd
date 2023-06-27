5af21195935bc7362563a02ca8038874dbac7b92
sumin <330sum@naver.com>
2023-06-27 11:25:19 +0900

    jwt 권한 프리픠리~

--- src/main/java/site/connectdots/connectdotsprj/jwt/config/JwtConfig.java
+++ src/main/java/site/connectdots/connectdotsprj/jwt/config/JwtConfig.java
@@ -17,11 +17,13 @@ public class JwtConfig implements WebMvcConfigurer {
         // TODO : URL 설정해야 함
         registry
                 .addInterceptor(jwtTokenInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/health-check")
-                .excludePathPatterns("/") // TODO : 메인페이지는 서버에서 알 수 없다. 이 경로가 맞나?
-                .excludePathPatterns("/connects/sign-up")
-                .excludePathPatterns("/connects/login")
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/health-check")
+//                .excludePathPatterns("/") // TODO : 메인페이지는 서버에서 알 수 없다. 이 경로가 맞나?
+//                .excludePathPatterns("/connects/sign-up")
+//                .excludePathPatterns("/connects/login")
+                .excludePathPatterns("/**")
+
         ;
     }
 }
